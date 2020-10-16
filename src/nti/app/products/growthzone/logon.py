6c9c26c13e08944deac4f131bf8d54bb15b4aac7
Josh Zuech <josh.zuech@nextthought.com>
2020-10-15 22:35:40 -0500

    Include scopes correctly

--- src/nti/app/products/growthzone/logon.py
+++ src/nti/app/products/growthzone/logon.py
@@ -98,7 +98,7 @@ def redirect_growthzone2_params(request, state=None):
               'response_type': 'code',
               'response_mode': 'form_post',
               'client_id': auth_settings.client_id,
-              'scope': 'openid+profile+email',
+              'scope': 'openid profile email',
                GROWTHZONE_RETURN_URL_PARAM: redirect_growthzone_uri(request)}
     return params
 
